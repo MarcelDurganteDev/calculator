99be537e53b4e1492e344f30d75050bfeaa5f252
MarcelDurgante <info@marceldurgante.dev>
2021-10-21 11:50:26 +0200

    div controls and toggle added to HTML

---
+++ main.js
@@ -0,0 +1,12 @@
+let mini = document.getElementById('mini');
+
+let btnSeven = document.getElementById('7').value;
+console.log(btnSeven);
+
+btnSeven.addEventListener('click', printMini);
+
+function printMini() {
+    btnSeven
+}
+
+
